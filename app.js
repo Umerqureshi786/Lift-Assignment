89aab0516f735ed9b9a5c9306f7da4d563ecce3b
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2024-08-04 11:33:23 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
-// alert("sssssss");
-// prompt("enter your name")
-// console.warn("My name is umer");
-// console.table("My name is umer=99");
 
+function groundFloor(){
+    const div = document.querySelector('floorGround');
+    div.style.transform = 'translateY(-100px)';
+}
+groundFloor()
+
+function floorOne(){
+    const div = document.querySelector('floorone');
+
+}
+floorOne()
+
+function floorTwo(){
+    const div = document.querySelector('floortwo');
+
+}
+floorTwo()
+
+
+function floorThree(){
+    const div = document.querySelector('floorthree');
+
+}
+floorThree()
+
+
+function floorFour(){
+    const div = document.querySelector('floorfour');
+}
+floorFour()
+
+
+function floorFive(){
+    const div = document.querySelector('floorfive');
+
+}
+floorFive()
+
+
+function floorSix(){
+    const div = document.querySelector('floorsix');
+}
+floorSix()
